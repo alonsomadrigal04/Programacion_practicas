92469dec443fbb5459cc3e14387731937c251556
alonsomadrigal04 <alonso.madrigal.h@gmail.com>
2022-11-13 11:27:31 +0100

    Intento ejercicio 10

--- Practica3/Ejercicio_10.py
+++ Practica3/Ejercicio_10.py
@@ -13,12 +13,28 @@ for fila in range(n):
 for fila in range(n):
     for columna in range(n):
         M[fila][columna] = int(input("Dame el valor ({0}, {1}): ". format(fila, columna)))
-
+        if M[fila][columna] < 1 or M[fila][columna] > n:
+            latino = False
+print(M[f][f+1:n])
 # Latino?
 while latino:
-    if M[f][c] < 1 or M[f][c] > n:
+    if M[f] in M[f][f+1:n]:
         latino = False
-    if M[f] in M[f][f:n]:
-        latino = False
-    for columna in range(n):
-        
+        print(latino, "2")
+    M_0 =[]
+    unico =[]
+    for fila in range(1, n):
+        M_0.append(M[fila][c])
+    for elemt in M_0:
+        if elemt not in unico:
+            unico.append(elemt)
+        else:
+            latino = False
+        print(latino, "3")
+
+    c += 1
+    f += 1
+if latino:
+    print("Es latino")
+else:
+    print("No lo es")
