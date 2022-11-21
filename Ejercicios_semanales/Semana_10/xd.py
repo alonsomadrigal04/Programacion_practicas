9c4c50e45639c107e0fb13739c056e5446c838c3
alonsomadrigal04 <alonso.madrigal.h@gmail.com>
2022-11-21 12:35:59 +0100

    Eliminación de hoja de pruebas

--- Ejercicios_semanales/Semana_10/xd.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt
-
-def area_triangulo(a, b, c):
-    s = (a + b + c) / 2
-    return sqrt(s * (s-a) * (s-b) * (s-c))
-s = 4
-print(area_triangulo(s-1, s, s+1))
-print(s)
-print(a)
